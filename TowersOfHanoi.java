bc92d9d1349613536f93b74681bdaabee5ab08eb
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2022-01-25 18:24:54 +0000

    Created and Finished TowersOfHanoi.java and style checked

---
+++ TowersOfHanoi.java
@@ -0,0 +1,64 @@
+/*
+ * This program reads the input.txt file and uses a recursive binary search function to
+ * find the index of a specific element. It will then output the index to output.txt.
+ *
+ * By Zefeng Wang
+ * Created on January 24, 2022*
+*/
+
+// import classes
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+// class TowersOfHanoi
+class TowersOfHanoi {
+
+  public static void main(String[] args) throws IOException {
+    
+    // Reads each line of the file
+    int line = 1;
+    List<String> lines = Files.readAllLines(Paths.get("input.txt"),
+                               StandardCharsets.UTF_8);
+    Iterator<String> itr = lines.iterator();
+
+    // Outputs the all of steps needed to beat the Tower of Hanoi and outputs error messages
+    if (lines.size() != 0) {
+      for (int i = 0; itr.hasNext(); i++) {
+        System.out.println("Output for Line " + line);
+        try {
+          int num = Integer.parseInt(itr.next());
+          if (num >= 0) {
+            hanoi(num, 1, 3);
+          } else {
+            System.out.println("Please enter a positive integer");
+          }
+        } catch (Exception e) {
+          System.out.println("Please enter an integer");
+        }
+        line++;
+      }
+    } else {
+      System.out.println("Please enter at least one number");
+    }
+  }
+
+  // Outputs the steps required to move num disks from 
+  // the start ped to the end peg
+  private static void hanoi(int num, int start, int end) {
+    if (num == 1) {
+      System.out.println("Move disk from peg " + start + " to peg " + end);
+    } else {
+      hanoi(num - 1, start, 6 - (start + end));
+      System.out.println("Move disk from peg " + start + " to peg " + end);
+      hanoi(num - 1, 6 - (start + end), end);
+    }
+  }
+} 
